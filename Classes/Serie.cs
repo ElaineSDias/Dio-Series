c04bcbc8b5c159d6b6b3ce85a2a7842c383939c8
ElaineSDias <emsilva.elaine@gmail.com>
2021-10-07 15:43:03 -0300

    projeto C#

---
+++ Classes/Serie.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace Dio.Series
+{
+    public class Serie : EntidadeBase
+    {
+        // Atributos
+		private Genero Genero { get; set; }
+		private string Titulo { get; set; }
+		private string Descricao { get; set; }
+		private int Ano { get; set; }
+        private bool Excluido {get; set;}
+
+        // MÃ©todos
+		public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+		{
+			this.Id = id;
+			this.Genero = genero;
+			this.Titulo = titulo;
+			this.Descricao = descricao;
+			this.Ano = ano;
+            this.Excluido = false;
+		}
+
+        public override string ToString()
+		{
+			// Environment.NewLine https://docs.microsoft.com/en-us/dotnet/api/system.environment.newline?view=netcore-3.1
+            string retorno = "";
+            retorno += "GÃªnero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "DescriÃ§Ã£o: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de InÃ­cio: " + this.Ano + Environment.NewLine;
+            retorno += "Excluido: " + this.Excluido;
+			return retorno;
+		}
+
+        public string retornaTitulo()
+		{
+			return this.Titulo;
+		}
+
+		public int retornaId()
+		{
+			return this.Id;
+		}
+        public bool retornaExcluido()
+		{
+			return this.Excluido;
+		}
+        public void Excluir() {
+            this.Excluido = true;
+        }
+    }
+}
